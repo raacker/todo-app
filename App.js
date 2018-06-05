883be85d083e25102ae8ef6007d44d5195919d58
raacker <haven.cpp@gmail.com>
2018-06-05 17:39:39 +0900

    add ToDo.js

--- App.js
+++ App.js
@@ -1,62 +1,85 @@
 import React from 'react';
-import {
-    StyleSheet,
-    Text,
-    View,
-    StatusBar,
-    TextInput,
-    Dimensions,
-    Platform,
-} from 'react-native';
+import { StyleSheet, Text, View, StatusBar, TextInput, Platform, ScrollView, Dimensions } from 'react-native';
+import ToDo from './ToDo';
 
 const { height, width } = Dimensions.get('window');
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <StatusBar barStyle="light-content" />
-        <Text style={styles.title}> TODO App</Text>
-        <View style={styles.card}>
-            <TextInput style={styles.input} placeholder={'New to do'} />
-        </View>
-      </View>
-    );
-  }
+	state = {
+		newToDo: '',
+	};
+	render() {
+		const { newToDo } = this.state;
+		return (
+			<View style={styles.container}>
+				<StatusBar barStyle="light-content" />
+				<Text style={styles.title}> TODO App</Text>
+				<View style={styles.card}>
+					<TextInput
+						style={styles.input}
+						value={newToDo}
+						placeholder={'New To Do'}
+						onChangeText={this._controlleNewToDo}
+						placeholderTextColor={'#999'}
+						returnKeyType={'done'}
+						autoCorrect={false}
+					/>
+					<ScrollView contentContainerStyle={styles.todo} >
+						<ToDo />
+					</ScrollView>
+				</View>
+			</View>
+		);
+	}
+
+	_controlleNewToDo = text => {
+		this.setState({
+			newToDo: text,
+		});
+	};
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#f23657',
-    alignItems: 'center',
-  },
-  title: {
-    color: 'white',
-    fontSize: 30,
-    marginTop: 50,
-    fontWeight: "200",
-    marginBottom: 30,
-  },
-  card: {
-      backgroundColor: 'white',
-      flex: 1,
-      width: width - 25,
-      borderTopLeftRadius: 10,
-      borderTopRightRadius: 10,
-      ...Platform.select({
-          ios: {
-            shadowColor: 'rgba(50, 50, 50, 1.0)',
-            shadowOpacity: 0.5,
-            shadowRadius: 5,
-            shadowOffset: {
-                height: -1,
-                width: 0,
-            },
-          },
-          android: {
-            elevation: 3,
-          },
-      })
-  }
+	container: {
+		flex: 1,
+		backgroundColor: '#f23657',
+		alignItems: 'center',
+	},
+	title: {
+		color: 'white',
+		fontSize: 30,
+		marginTop: 50,
+		fontWeight: '200',
+		marginBottom: 30,
+	},
+	card: {
+		backgroundColor: 'white',
+		flex: 1,
+		width: width - 25,
+		borderTopLeftRadius: 10,
+		borderTopRightRadius: 10,
+		...Platform.select({
+			ios: {
+				shadowColor: 'rgba(50, 50, 50, 1.0)',
+				shadowOpacity: 0.5,
+				shadowRadius: 5,
+				shadowOffset: {
+					height: -1,
+					width: 0,
+				},
+			},
+			android: {
+				elevation: 3,
+			},
+		}),
+	},
+	input: {
+		padding: 20,
+		borderBottomColor: '#bbb',
+		borderBottomWidth: 1,
+		fontSize: 25,
+    },
+    todo: {
+        alignItems: 'center',
+    },
 });
