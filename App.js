16e052b167410567a55289cd96f37d7fb258fcc2
raacker <haven.cpp@gmail.com>
2018-06-05 15:55:45 +0900

    add card and textinput

--- App.js
+++ App.js
@@ -1,11 +1,25 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+    StyleSheet,
+    Text,
+    View,
+    StatusBar,
+    TextInput,
+    Dimensions,
+    Platform,
+} from 'react-native';
+
+const { height, width } = Dimensions.get('window');
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
+        <StatusBar barStyle="light-content" />
+        <Text style={styles.title}> TODO App</Text>
+        <View style={styles.card}>
+            <TextInput style={styles.input} placeholder={'New to do'} />
+        </View>
       </View>
     );
   }
@@ -14,8 +28,35 @@ export default class App extends React.Component {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#f23657',
     alignItems: 'center',
-    justifyContent: 'center',
   },
+  title: {
+    color: 'white',
+    fontSize: 30,
+    marginTop: 50,
+    fontWeight: "200",
+    marginBottom: 30,
+  },
+  card: {
+      backgroundColor: 'white',
+      flex: 1,
+      width: width - 25,
+      borderTopLeftRadius: 10,
+      borderTopRightRadius: 10,
+      ...Platform.select({
+          ios: {
+            shadowColor: 'rgba(50, 50, 50, 1.0)',
+            shadowOpacity: 0.5,
+            shadowRadius: 5,
+            shadowOffset: {
+                height: -1,
+                width: 0,
+            },
+          },
+          android: {
+            elevation: 3,
+          },
+      })
+  }
 });
